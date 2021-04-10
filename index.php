d4f4fa336e82e95ce832e9926f81446c8e788b12
Vesnovey <ilinna2005@gmail.com>
2021-04-10 23:47:41 +0300

    upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$token = "1603733512:AAEaVyYhnCzLnBzo11Ld8xSf75CsLso9Mv8";
+
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
