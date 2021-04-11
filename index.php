b417f8703a41afe39d22379abc6ef980d7eacf43
Vesnovey <ilinna2005@gmail.com>
2021-04-11 22:42:58 +0300

    Variety of answers for unknown messages

--- index.php
+++ index.php
@@ -15,10 +15,19 @@
     /help - вывод справки';
         $bot->sendMessage($message->getChat()->getId(), $answer);
     });
+
+    // вариации ответов на неизвестные сообщения
     $bot->on(function (\TelegramBot\Api\Types\Update $update) use ($bot) {
+        $array = [
+            "Ты нормальный вообще?" ,
+            "Пиши адекватные вещи" ,
+            "Я не понимаю" ,
+            "Я не могу выполнить эту команду" ,
+        ];
         $message = $update->getMessage();
         $id = $message->getChat()->getId();
-        $bot->sendMessage($id, 'Ты нормальный вообще?' );
+        $rand_keys = array_rand($array, 1);
+        $bot->sendMessage($id, $array[$rand_keys[0]] );
     }, function () {
         return true;
     });
