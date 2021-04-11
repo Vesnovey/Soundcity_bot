081fe59e89223ff340cf1a35a988e5de292fc703
Vesnovey <ilinna2005@gmail.com>
2021-04-11 20:42:31 +0300

    bug:libs required  fixed

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
+    require_once "vendor/autoload.php";
+    $token = "1603733512:AAEaVyYhnCzLnBzo11Ld8xSf75CsLso9Mv8";
 
-$token = "1603733512:AAEaVyYhnCzLnBzo11Ld8xSf75CsLso9Mv8";
+    $bot = new \TelegramBot\Api\BotApi($token);
+    // команда для start
+    $bot->command('start', function ($message) use ($bot) {
+        $answer = 'Добро пожаловать!';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
 
-$bot = new \TelegramBot\Api\Client($token);
-// команда для start
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
+    // команда для помощи
+    $bot->command('help', function ($message) use ($bot) {
+        $answer = 'Команды:
+    /help - вывод справки';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
 
-// команда для помощи
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - вывод справки';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-$bot->run();
+    $bot->run();
 
